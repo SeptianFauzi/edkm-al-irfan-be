c08b27e33582e91cb0494e8dafb69c51c07dd057
Septian <fauzy434@gmail.com>
2026-03-14 10:53:00 +0700

    refactor: Remove try-catch block and dynamically calculate the next year for `is_money_box_sent` updates.

--- app/Http/Controllers/CelenganController.php
+++ app/Http/Controllers/CelenganController.php
@@ -145,7 +145,6 @@ class CelenganController extends Controller
             $data = $validated->messages();
             return response()->json(['status' => $status, 'message' => $message, 'data' => $data], 500);
         } else {
-            try {
             $celenganGet = Celengan::where('id', $id)->where('year_hijriah', $request->year_hijriah)->first();
             if ((!is_null($request->is_money_received) && !is_null($request->is_money_box_sent)) && ($celenganGet->is_money_received != $request->is_money_received || $celenganGet->amount != $request->amount) && ($celenganGet->is_money_box_sent != $request->is_money_box_sent)) {
                 $data = array(
@@ -191,7 +190,7 @@ class CelenganController extends Controller
             $celenganUpdate = Celengan::where('id', $celenganGet->id)->update($data);
 
             if($request->isForNextYear){
-                $celenganUpdate = Celengan::where('id_peserta', $request->id_peserta)->where("year_hijriah", 1448)->update(array('is_money_box_sent' => 1));
+                $celenganUpdate = Celengan::where('id_peserta', intval($request->id_peserta))->where("year_hijriah", (intval($request->year_hijriah))+1)->update(array('is_money_box_sent' => 1));
             }
             if ($celenganUpdate) {
                 $status = 'Success';
@@ -204,14 +203,8 @@ class CelenganController extends Controller
                 $data = $data;
                 return response()->json(['status' => $status, 'message' => $message, 'data' => $data], 500);
             }
-        } catch (\Exception $e) {
-            $status = 'Failed';
-            $message = 'Data Not Updated';
-            $data = $data;
-            return response()->json(['status' => $status, 'message' => $e, 'data' => $data], 500);
         }
     }
-}
 
     /**
      * Remove the specified resource from storage.
